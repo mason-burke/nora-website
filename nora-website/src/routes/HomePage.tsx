68117a0eb4ec511160b4d7adb153be8606129b0d
Mason Burke <masontheburke@gmail.com>
2025-08-27 14:28:10 -0400

    fix infinite rerender issue

--- nora-website/src/routes/HomePage.tsx
+++ nora-website/src/routes/HomePage.tsx
@@ -1,12 +1,3 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 export const HomePage = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (window.location.pathname != '/') navigate('/');
-  });
-
   return <div className="main-content">hello world</div>;
 };
